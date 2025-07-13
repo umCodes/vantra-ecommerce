38759c416e4d4bd8f0ee71f71859ed446f71fd42
umCodes <umohammed.official@gmail.com>
2025-07-13 06:32:01 +0300

    Update ProductsList.jsx

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,17 +1,21 @@
 import { useContext } from "react";
 import ProductCard from "./ProductCard";
 import { ProductsContext } from "../context/ProductsProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function ProductsList({ products }) {
-  const [ _, _, isLoading ] = useContext(ProductsContext); // Get products from context
+  const [_, _, isLoading] = useContext(ProductsContext); // [products, setProducts, isLoading]
 
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center text-2xl text-gray-500">
+        <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
+        Loading products...
+      </div>
+    );
+  }
 
-    if(isLoading){
-        return (<div className='h-full flex items-center justify-center' >
-              <span>loading products...</span>  
-            </div>)
-    }
-    
   return (
     <div className="px-8 py-4 overflow-y-scroll max-h-[80vh]">
       <h2 className="text-3xl font-bold mb-4">Products</h2>
